b72c514a4ec137c24fa352418de4e1a0e26dc0e1
rffgrayson <Ariffazman485@gmail.com>
2026-03-14 16:35:24 +0800

    fix: add Content component in the App.jsx

--- part1/exercise-react/src/App.jsx
+++ part1/exercise-react/src/App.jsx
@@ -1,33 +1,38 @@
-const Header = () => {
-    const course = 'Half Stack application development'
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
 
-    return (
-      <div>
-        <h1>{course}</h1>
-      </div>
-    )
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+  return (
+    <div>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+    </div>
+  )
 }
 
 const App = () => {
+  const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+  
   return (
     <div>
-      <Header />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )

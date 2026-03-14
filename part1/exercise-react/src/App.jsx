529dd3d864de62325ce9236873346c5168c2c329
rffgrayson <Ariffazman485@gmail.com>
2026-03-14 16:48:17 +0800

    feats: add Part component

--- part1/exercise-react/src/App.jsx
+++ part1/exercise-react/src/App.jsx
@@ -6,12 +6,20 @@ const Header = ({ course }) => {
   )
 }
 
+const Part = ({name, exercises}) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
+    </div>
+  )
+}
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
     </div>
   )
 }

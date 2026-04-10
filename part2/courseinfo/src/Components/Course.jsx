b515736c503db497af9d5469e1c804c4feccabb2
rffgrayson <Ariffazman485@gmail.com>
2026-04-10 12:00:09 +0800

    feat(2.5): extract Course component into separate module

---
+++ part2/courseinfo/src/Components/Course.jsx
@@ -0,0 +1,46 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>{part.name} {part.exercises}</p>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+     {parts.map(part =>
+            <Part key={part.id} part={part}/>
+        )
+      }     
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((a, part) => a + part.exercises, 0)
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course

ea4d632c333c2a486edb5b0aacbed827540632ad
rffgrayson <Ariffazman485@gmail.com>
2026-04-10 11:24:07 +0800

    feat(2.1-2.3): render course with map and calculate total with reduce

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -26,14 +26,16 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
+  console.log(parts)
+  const sum = parts.reduce((a, part) => a + part.exercises, 0)
   return (
-    <p>
-      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
+    <p>Number of exercises {sum}</p>
   )
 }
 
 const Course = ({ course }) => {
+  console.log(course)
+  console.log(course.parts)
   return (
     <div>
       <Header course={course.name} />

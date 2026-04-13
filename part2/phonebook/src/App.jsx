a8ee46ff9c83d35bda968315698a1ac05ee51b67
rffgrayson <Ariffazman485@gmail.com>
2026-04-14 04:33:39 +0800

    feats(2.9): Implement search filter for phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '01232424' }
-  ]) 
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterPerson, setFilterPerson] = useState('')
 
   const addName = (event) => {
     event.preventDefault() 
@@ -21,15 +25,29 @@ const App = () => {
 
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
 
     setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')
   }
+
+ const personsToShow = filterPerson === '' ? persons : persons.filter(person => 
+      person.name.toLowerCase().includes(filterPerson.toLowerCase()))
+
   return (
     <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addName}>
+        <div>
+          filter shown with <input 
+                    value={filterPerson} 
+                    onChange={(event) => setFilterPerson(event.target.value)} 
+                />
+        </div>
+      </form>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
@@ -50,11 +68,11 @@ const App = () => {
       </form>
       
       <h2>Numbers</h2>
-       <ul>
-      {persons.map((person) => (
-       <li key={person.name}>{person.name} {person.number}</li>
-      ))}
-    </ul>
+      <ul>
+        {personsToShow.map(person => 
+          <li key={person.id}>{person.name} {person.number}</li>
+        )}
+      </ul>
     </div>
   )
 }

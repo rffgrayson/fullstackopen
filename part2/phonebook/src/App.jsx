1acb79f18b737c86812a5cb65bd769ee6873099a
rffgrayson <Ariffazman485@gmail.com>
2026-04-14 03:26:27 +0800

    feat(2.8): add phone number field to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: '01232424' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault() 
@@ -19,11 +20,13 @@ const App = () => {
     }
 
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
   return (
     <div>
@@ -36,13 +39,20 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input 
+                    value={newNumber} 
+                    onChange={(event) => setNewNumber(event.target.value)} 
+                />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
        <ul>
       {persons.map((person) => (
-       <li key={person.name}>{person.name}</li>
+       <li key={person.name}>{person.name} {person.number}</li>
       ))}
     </ul>
     </div>
